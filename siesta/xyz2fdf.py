677a93dcc4c0536f6d87ae9112ab7bd0f6dc3df9
DeqiTang <deqi_tang@163.com>
2019-09-10 19:45:37 +0800

    add xyz2fdf.py

---
+++ siesta/xyz2fdf.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+import numpy as np
+import sys
+import pymatgen as mg
+
+
+"""
+Usage:
+    python xyz2fdf xxx.xyz xxx.fdf
+    xxx.xyz is the input structure file
+    xxx.fdf is the generated SIESTA input file
+"""
+
+class Atom:
+    """
+    a representation of atom with xyz coordinates
+    """
+    def __init__(self, name=None, x=0, y=0, z=0):
+        self.name = name
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+    def set_name(self, name):
+        self.name = name
+    def set_x(self, x):
+        self.x = float(x)
+    def set_y(self, y):
+        self.y = float(y)
+    def set_z(self, z):
+        self.z = float(z)
+
+
+class XYZ:
+    """
+    a representation of xyz file
+    """
+    def __init__(self, xyz_f=sys.argv[1]):
+        self.file = xyz_f
+        self.natom = 0
+        self.nspecies = 0
+        self.atoms = []
+        self.specie_labels = dict()
+        self.get_info()
+
+    def get_info(self):
+        with open(self.file, 'r') as fin:
+            self.natom = int(fin.readline())
+            fin.readline()
+            i = 0
+            while i < self.natom:
+                line = fin.readline()
+                atom = Atom(line.split()[0], float(line.split()[1]), float(line.split()[2]), float(line.split()[3]))
+                self.atoms.append(atom)
+                i += 1
+        self.set_species_number()
+
+    def set_species_number(self):
+        names = [self.atoms[x].name for x in range(self.natom)]
+        species = set(names)
+        species = list(species)
+        species_with_order = {}
+        for i in species:
+            species_with_order[i] = mg.Element(i).number
+        tmp = sorted(zip(species_with_order.values(), species_with_order.keys()))
+        for i in range(len(tmp)):
+            tmp[i] = list(tmp[i])
+        for i in range(len(tmp)):
+            tmp[i][0] = i + 1
+        tmp = dict(tmp)
+        self.specie_labels = dict(zip(tmp.values(), tmp.keys()))
+        self.nspecies = len(self.specie_labels)
+
+    def to_fdf(self, fname):
+        with open(fname, 'a') as fout:
+            fout.write("%block ChemicalSpeciesLabel\n")
+            for element in self.specie_labels:
+                fout.write("\t%d\t%d\t%s\n" % (self.specie_labels[element], mg.Element(element).number, element))
+            fout.write("%endblock ChemicalSpeciesLabel\n")
+            fout.write("\n")
+
+            fout.write("%block PAO.BasisSizes\n")
+            for element in self.specie_labels:
+                fout.write("\t%s\tDZP\n" % element)
+            fout.write("%endblock PAO.BasisSizes\n")
+            fout.write("\n")
+
+            fout.write("AtomicCoordinatesFormat ScaledCartesian\n")
+            fout.write("AtomCoorFormatOut Ang\n")
+            fout.write("LatticeConstant 1.00000 Ang\n")
+            fout.write("\n")
+
+            fout.write("%block LatticeVectors\n")
+            fout.write("\n")
+            fout.write("%endblock LatticeVectors\n")
+            fout.write("\n")
+
+            fout.write("%block AtomicCoordinatesAndAtomicSpecies\n")
+            for atom in self.atoms:
+                fout.write("%f\t%f\t%f\t" % (atom.x, atom.y, atom.z))
+                fout.write(str(xyz.specie_labels[atom.name]))
+                fout.write("\n")
+            fout.write("%endblock AtomicCoordinatesAndAtomicSpecies\n")
+            fout.write("\n")
+
+    def update(self, newxyzfile):
+        self.file = newxyzfile
+        self.natom = 0
+        self.nspecies = 0
+        self.atoms = []
+        self.specie_labels = dict()
+        self.get_info()
+        self.set_species_number()
+
+
+       
+        
+
+# OK now we can use XYZ class to extract information 
+# from the xyz file: sys.argv[1]
+
+xyz = XYZ()
+
+system_name = "LiH"
+system_label = "LiH"
+
+with open(sys.argv[2], 'w') as fout:
+        fout.write("SystemName\t%s\n" % system_name)
+        fout.write("SystemLabel\t%s\n" % system_label)
+        fout.write("NumberOfSpecies\t%d\n" %  xyz.nspecies)
+        fout.write("NumberOfAtoms\t%d\n" % xyz.natom)        
+        fout.write("\n")
+
+xyz.to_fdf(sys.argv[2])
+
+
+#---------------------------------
+# DFT settings:
+# --------------------------------
+with open(sys.argv[2], 'a') as fout:
+    fout.write("# DFT settings\n")
+    fout.write("\n")
+    fout.write("XC.functional  GGA\n")
+    fout.write("XC.author  PBE\n")
+    fout.write("MeshCutoff  100.0 Ry\n")
+    fout.write("DM.Tolerance  1.d-6\n")
+    fout.write("MaxSCFIterations  300\n")
+    fout.write("DM.MixingWeight  0.1\n")
+    fout.write("DM.NumberPulay  5\n")
+    fout.write("DM.AllowExtrapolation  true\n")
+    fout.write("DM.UseSaveDM  true\n")
+    fout.write("SolutionMethod diagon\n")
+    fout.write("\n")
+
+#-------------------------------------
+# MD settings:
+#-------------------------------------
+with open(sys.argv[2], 'a') as fout:
+    pass
