1bd295456030b91fd69a9ba15049bb9121824219
DeqiTang <openview@163.com>
2020-02-16 20:27:52 +0800

    modifying docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../pyamtflow"))
+#sys.path.insert(0, os.path.abspath("../pyamtflow"))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
