7fa3f79b64dfd97ea7a39aeaa9569e9f92614add
DeqiTang <deqi_tang@163.com>
2020-07-12 20:48:29 +0800

    Unit of value is verified in chg-diff-1d.py

--- pymatflow/scripts/chg-diff-1d.py
+++ pymatflow/scripts/chg-diff-1d.py
@@ -103,54 +103,88 @@ def main():
     # data_iii dimension reduction
     data_sub = np.array(data_iii[0]) - np.array(data_iii[1]) - np.array(data_iii[2])
     # the unit of value is actually not physical now!
+    cell_volume = np.dot(np.cross(np.array(structure.cell[0]), np.array(structure.cell[1])), np.array(structure.cell[2]))
+    cell_volume_per_unit = cell_volume / (ngzf * ngyf * ngxf)
     
+    # value in Vasp *CHG* are \rho(r)_of_electrons * Volume_of_cell, so we should divide it by cell_volume here and time it with cell_volume_per_unit
+    # to get the number of electrons per divided unit
+    total_electrons = np.sum(data_iii[0]) / cell_volume * cell_volume_per_unit
+    #
+    
+    print("======================================================\n")
+    print("           Information collected\n")
+    print("------------------------------------------------------\n")
+    print("cell volume: %f (A^3)\n" % cell_volume)
+    print("total electrons: %f\n" % total_electrons)
+    
+    
+    
+    # unit of data_red_? is e/Anstrom, namely number of electrons per Angstrom
     data_red_a = []
     data_red_b = []
     data_red_c = []
     if "c" in args.abscissa:
+        factor = cell_volume_per_unit / cell_volume
+        len_ci = c / ngzf
         for ci in range(data_sub.shape[0]):
             tmp = 0
             for bi in range(data_sub.shape[1]):
                 tmp += np.sum(data_sub[ci, bi, :])
-            data_red_c.append(tmp)
+            nelect_ci = tmp * factor
+            rho_line = nelect_ci / len_ci
+            data_red_c.append(rho_line)
     if "b" in args.abscissa:
+        factor = cell_volume_per_unit / cell_volume
+        len_bi = b / ngyf    
         for bi in range(data_sub.shape[1]):
             tmp = 0
             for ai in range(data_sub.shape[2]):
                 tmp += np.sum(data_sub[:, bi, ai])
-            data_red_b.append(tmp)
+            nelect_bi = tmp * factor
+            rho_line = nelect_bi / len_bi                
+            data_red_b.append(rho_line)
     if "a" in args.abscissa:
+        factor = cell_volume_per_unit / cell_volume
+        len_ai = a / ngxf        
         for ai in range(data_sub.shape[2]):
             tmp = 0
             for ci in range(data_sub.shape[0]):
                 tmp += np.sum(data_sub[ci, :, ai])
-            data_red_a.append(tmp)    
+            nelect_ai = tmp * factor
+            rho_line = nelect_ai / len_ai                   
+            data_red_a.append(rho_line)    
 
     # output the data and make the plot
     if "c" in args.abscissa:
-        plt.plot(np.linspace(0, c, len(data_red_c)), data_red_c)
         with open(args.output+".1d.c.data", 'w') as fout:
+            fout.write("#c(angstrom) \Delta(rho) (number of electron per Angstrom)\n")
             c_coord = np.linspace(0, c, len(data_red_c))
             for i in range(len(data_red_c)):
                 fout.write("%f %f\n" % (c_coord[i], data_red_c[i]))
+        plt.plot(np.linspace(0, c, len(data_red_c)), data_red_c)                
+        plt.ylabel(r"$\Delta\rho (e/\AA)$")
         plt.tight_layout()
         plt.savefig(args.output+".1d.c.png")
         plt.close()                
     if "b" in args.abscissa:
-        plt.plot(np.linspace(0, b, len(data_red_b)), data_red_b)    
         with open(args.output+".1d.b.data", 'w') as fout:
+            fout.write("#b(angstrom) \Delta(rho) (number of electron per Angstrom)\n")
             b_coord = np.linspace(0, b, len(data_red_b))
             for i in range(len(data_red_b)):
                 fout.write("%f %f\n" % (b_coord[i], data_red_b[i]))        
+        plt.plot(np.linspace(0, b, len(data_red_b)), data_red_b)    
+        plt.ylabel(r"$\Delta\rho (e/\AA)$")              
         plt.tight_layout()
         plt.savefig(args.output+".1d.b.png")
         plt.close()                
     if "a" in args.abscissa:
-        plt.plot(np.linspace(0, a, len(data_red_a)), data_red_a)
         with open(args.output+".1d.a.data", 'w') as fout:
+            fout.write("#a(angstrom) \Delta(rho) (number of electron per Angstrom)\n")
             a_coord = np.linspace(0, a, len(data_red_a))
             for i in range(len(data_red_a)):
                 fout.write("%f %f\n" % (a_coord[i], data_red_a[i]))
+        plt.plot(np.linspace(0, a, len(data_red_a)), data_red_a)                
+        plt.ylabel(r"$\Delta\rho (e/\AA)$")            
         plt.tight_layout()
         plt.savefig(args.output+".1d.a.png")
         plt.close()
