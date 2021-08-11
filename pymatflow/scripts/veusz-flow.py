12389014687f186e4bffeb5a993c5f3195e7bbaf
DeqiTang <deqi_tang@163.com>
2021-08-11 13:00:21 +0800

    update veusz-flow.py

--- pymatflow/scripts/veusz-flow.py
+++ pymatflow/scripts/veusz-flow.py
@@ -5,19 +5,34 @@ import sys
 import copy
 import argparse
 
-import veusz.embed as veusz_embed
+import veusz.embed as veusz_embed 
 import time
 
 """
+using veusz in python via 'embed' mode
+Compatible with Veusz 3.3.1
 Note:
     if running in WLS, you need to let Qt not launch the window by:
         export QT_QPA_PLATFORM='offscreen'
+
+Dependencies:
+    pip install --user veusz
+Ref:
+    * https://veusz.github.io/docs/manual/api.html
+    * https://github.com/veusz/veusz/wiki
+    * https://github.com/jeremysanders/veusz/wiki/EmbeddingPython
+    * https://github.com/veusz/veusz/wiki/ToolsPlugins
 """
 
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
-def bandDataToVeusz(datafile, gplotfile, vszfile):
+def bandDataToVeuszEmbed(datafile, gplotfile, vszfile):
+    """
+    Note:
+        datafile and gplotfile are files usually generated in post-processing dir by pymatflow
+    """
+    #os.environ["QT_QPA_PLATFORM"] = "offscreen"
     # First you should import the band data to veusz with name 'data' as a nD dataset.
-
     #  open a new window and return a new Embedded object
     embed = veusz_embed.Embedded('window title', hidden=True)
     
@@ -40,6 +55,11 @@ def bandDataToVeusz(datafile, gplotfile, vszfile):
 
     #  make a new page, but adding a page widget to the root widget
     page = embed.Root.Add('page')
+    # if you run python in Veusz GUI application at console windows,
+    # there is a default global 'Root' already which handle the current GUI.
+    # and the command like ImportFileND are all available.
+    # However, we adopt 'embed' mode here, all the above are available under
+    # the embed.Embedded() class.
 
     # note: autoAdd=False stops graph automatically adding own axes (used in saved files)
     graph = page.Add('graph', autoadd=False)
@@ -110,21 +130,31 @@ def bandDataToVeusz(datafile, gplotfile, vszfile):
         label.label.val = item["label"]
         label.xPos.val = [item["x"]]
         label.yPos.val = [graph.y.min.val-0.5]
+    # and Fermi horizontal line
+    line = graph.Add("line")
+    line.mode.val = "length-angle"
+    line.positioning.val = "relative"
+    line.angle.val = [0]
+    line.xPos.val = [0]
+    line.yPos.val = [abs(0-graph.y.min.val) / (graph.y.max.val - graph.y.min.val)]
+    line.length.val = [1]
+    line.Line.style.val = "dashed"
+    line.Line.width.val = "0.5pt"
 
     graph.x.autoExtend.val = False
     graph.x.autoExtendZero.val = False
 
     # save to veusz project file
     embed.Save(vszfile)
-    embed.Export("%s.png" % vszfile, color=True, page=-1, dpi=300, antialias=True, quality=85, backcolor='#ffffff00', pdfdpi=150, svgtextastext=False)
+    embed.Export("%s.png" % vszfile, color=True, page=-1, dpi=300, antialias=True, quality=100, backcolor='#ffffff00', pdfdpi=150, svgtextastext=False)
 
-
+    #del os.environ["QT_QPA_PLATFORM"]
 
 def main():
     parser = argparse.ArgumentParser()
     
     parser.add_argument("-i", "--input", type=str, nargs=2, required=True,
-        help="input data file and gnuplot file, -i TOTAL BANDDATA GNUPLOTFILE. if running in WLS, you need to let Qt not launch the window by: export QT_QPA_PLATFORM=\'offscreen\'")
+        help="input data file and gnuplot file usually found in post-processing dir from pymatflow, -i TOTAL BANDDATA GNUPLOTFILE. if running in WLS, you need to let Qt not launch the window by: export QT_QPA_PLATFORM=\'offscreen\'")
 
     parser.add_argument("--output-vsz", type=str, default="./auto-output.vsz",
         help="save to an veusz project file")
@@ -133,7 +163,7 @@ def main():
     #
     #
 
-    bandDataToVeusz(
+    bandDataToVeuszEmbed(
         datafile=args.input[0],
         gplotfile=args.input[1],
         vszfile=args.output_vsz)
